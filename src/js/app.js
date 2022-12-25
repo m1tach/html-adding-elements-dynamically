a04f12c9860009150053cded36bf50c0f57279b5
Dimitar Malinov <mitachftw@gmail.com>
2022-12-25 14:41:34 +0200

    fix: code in script tag

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener('click', attachArticles());
-  
-  function attachArticles() {
-    for (let i = 0; i < 5; i++) {
-      const article = document.createElement('article');
-      article.classList.add('message');
-      article.textContent = 'sample text';
-      document.body.appendChild(article);
-    }
-  }
+  // This block will be executed once the page is loaded and ready
+
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("💣");
+  });
 });
