dcb7b16e848f73e65e69f6ae3cd12d097c99e239
Dimitar Malinov <mitachftw@gmail.com>
2022-12-25 14:30:41 +0200

    add: logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  document.body.addEventListener('click', attachArticles());
+  
+  function attachArticles() {
+    for (let i = 0; i < 5; i++) {
+      const article = document.createElement('article');
+      article.classList.add('message');
+      article.textContent = 'sample text';
+      document.body.appendChild(article);
+    }
+  }
 });
